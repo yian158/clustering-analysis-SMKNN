d5645e2a37ba6e0c2e649ccbf6a617c1e022895d
yian158 <48200604+yian158@users.noreply.github.com>
2022-11-13 18:27:56 +0800

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -4,11 +4,11 @@ import numpy as np
 
 if __name__ == "__main__":
     # Data sets to be clustered (Rows of X correspond to points, columns correspond to variables)
-    input_file_name = './datasets/Glass.mat'
+    input_file_name = './datasets/Sticks.mat'
     # K : The number of clusters
-    K = 6
+    K = 4
 
-    dataset = read_file(input_file_name)
     # cluster
+    dataset = read_file(input_file_name)
     clusters, label = cluster(dataset, K, ka=10, la=1, si=(1/np.power(2, 0.5)))
 
